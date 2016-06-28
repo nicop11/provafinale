c475fa9238c5362c88a93129db1fde3e8c78e870
nicop11 <nicholasxm88@yahoo.it>
2016-06-28 21:26:03 +0200

    OpenCV added

--- app/src/main/java/np/it/provafinale/MainActivity.java
+++ app/src/main/java/np/it/provafinale/MainActivity.java
@@ -1,13 +1,63 @@
 package np.it.provafinale;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private JavaCameraView mJavaCameraView;
+    private static final int PERMISSION_CAMERA = 1;
+
+    static {
+        if (!OpenCVLoader.initDebug())
+            Log.d("ERROR", "Unable to load OpenCV");
+        else
+            Log.d("SUCCES", "OpenCV loaded");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mJavaCameraView = (JavaCameraView) findViewById(R.id.cameraView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            mJavaCameraView.enableView();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mJavaCameraView.disableView();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CAMERA:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    mJavaCameraView.enableView();
+                break;
+        }
     }
 }
+
